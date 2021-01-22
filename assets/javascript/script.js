fc72d339a923a5ffea355016bf571dbe77e73115
dave vebber <davevebber@gmail.com>
2021-01-21 20:35:48 -0800

    added varialbes and current time to top. created HTML and CSS

---
+++ assets/javascript/script.js
@@ -0,0 +1,41 @@
+// displays current time
+let currentDate = document.getElementById('current-date').innerHTML = "Current Time and Date: " + (moment().format('ddd, MMM Do, hh:mm a'));
+
+// current hour
+let currentHour = JSON.stringify(moment().format('H'));
+console.log(currentHour);
+
+// textarea
+let textarea9 = document.querySelector("#textarea9")
+let textarea10 = document.querySelector("#textarea10")
+let textarea11 = document.querySelector("#textarea11")
+let textarea12 = document.querySelector("#textarea12")
+let textarea1 = document.querySelector("#textarea1")
+let textarea2 = document.querySelector("#textarea2")
+let textarea3 = document.querySelector("#textarea3")
+let textarea4 = document.querySelector("#textarea4")
+let textarea5 = document.querySelector("#textarea5")
+
+// time blocks
+const nine = document.querySelector("#nine-am");
+const ten = document.querySelector('#ten-am');
+const eleven = document.querySelector('#eleven-am');
+const twelve = document.querySelector('#twelve-pm');
+const one = document.querySelector('#one-pm');
+const two = document.querySelector('#two-pm');
+const three = document.querySelector('#three-pm');
+const four = document.querySelector('#four-pm');
+const five = document.querySelector('#five-pm');
+
+// save btns
+const saveBtn9 = document.querySelector('#save-btn9')
+const saveBtn10 = document.querySelector('#save-btn10')
+const saveBtn11 = document.querySelector('#save-btn11')
+const saveBtn12 = document.querySelector('#save-btn12')
+const saveBtn1 = document.querySelector('#save-btn1')
+const saveBtn2 = document.querySelector('#save-btn2')
+const saveBtn3 = document.querySelector('#save-btn3')
+const saveBtn4 = document.querySelector('#save-btn4')
+const saveBtn5 = document.querySelector('#save-btn5')
+
+
